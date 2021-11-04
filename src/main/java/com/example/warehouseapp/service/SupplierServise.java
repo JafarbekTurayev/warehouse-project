92def8466446cffbf8c4779f8a3462be9d0ea0f2
ShoxCoder <shahzodqudratov6880@gmail.com>
2021-11-04 19:15:37 +0500

    shahzod commit

---
+++ src/main/java/com/example/warehouseapp/service/SupplierServise.java
@@ -0,0 +1,43 @@
+package com.example.warehouseapp.service;
+
+import com.example.warehouseapp.entity.Supplier;
+import com.example.warehouseapp.payload.ApiResponse;
+import com.example.warehouseapp.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SupplierServise {
+    @Autowired
+    SupplierRepository supplierRepository;
+
+    public ApiResponse add(Supplier supplier) {
+        if (!supplierRepository.existsByPhoneNumber(supplier.getName())) {
+            supplierRepository.save(supplier);
+            return new ApiResponse("Saved!", true);
+        }
+        return new ApiResponse("this supplier already exists", false);
+    }
+
+    public ApiResponse getOne(Integer id) {
+        Optional<Supplier> onId = supplierRepository.findById(id);
+        if (!onId.isPresent()) return new ApiResponse("Not found!", false);
+
+        return new ApiResponse("Mana", true, onId.get());
+
+
+    }
+
+    public ApiResponse edit(Integer id, Supplier supplier) {
+        Optional<Supplier> onId = supplierRepository.findById(id);
+        if (!onId.isPresent()) return new ApiResponse("Not found!", false);
+
+       Supplier edit = onId.get();
+        edit.setName(supplier.getName());
+        supplierRepository.save(edit);
+        return new ApiResponse("Edited", true, edit);
+    }
+
+}

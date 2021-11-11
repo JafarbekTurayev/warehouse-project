6dba1155f1f91f87647e56a81cdcc25786b2b303
AkmalMaxmudov <mahmudovakmaljon70@gmail.com>
2021-11-12 01:46:10 +0500

    RoleController and RoleService finished

--- src/main/java/com/example/warehouseapp/controller/RoleController.java
+++ src/main/java/com/example/warehouseapp/controller/RoleController.java
@@ -6,11 +6,9 @@ import com.example.warehouseapp.payload.responce.InputDTO;
 import com.example.warehouseapp.service.RolesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
@@ -27,7 +25,32 @@ public class RoleController {
         return ResponseEntity.ok(response);
     }
 
-//edit
+
+    @GetMapping
+    public HttpEntity<?> getAll(@RequestParam int page, @RequestParam int size){
+        ApiResponse apiResponse = rolesService.getAll(page, size);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping("/{id}")
+    public HttpEntity<?> getOne(@PathVariable Integer id){
+        Role one = rolesService.getOne(id);
+        return ResponseEntity.ok(one);
+    }
+
+    @PutMapping("/{id}")
+    public HttpEntity<?> edit(@PathVariable Integer id, @RequestBody Role dto){
+        ApiResponse edit = rolesService.edit(id, dto);
+        return ResponseEntity.status(edit.isSuccess()? 201 : 409).body(edit);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpEntity<?> delete(@PathVariable Integer id){
+        ApiResponse delete = rolesService.delete(id);
+        return ResponseEntity.status(delete.isSuccess() ? 204 : 409).body(delete);
+    }
+
+    //edit
     //getONe
     //getAll
     //Akmal
